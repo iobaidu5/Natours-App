d13fd5efe683d9628d5a24ff5082144b4187e2df
iobaidu5 <i.obaidu5@gmail.com>
2020-11-30 22:48:52 -0800

    API Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,7 +27,20 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req,res) => { 
     try{
-        const tours = await Tour.find();
+        // BUILD QUERY
+    // 1) Filtering
+        const queryObj = { ...req.query };
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+        excludeFields.forEach(el => delete queryObj[el]);
+    // 2) Advance Filtering
+        let queryStr = JSON.stringify(queryObj);
+        quertStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+         console.log(JSON.parse(queryStr));
+
+
+        const query =  Tour.find(queryObj);
+        const tours = await query; // EXECUTE QUERY
+
         res.status(200).json({
             status: "success",
             results: tours.length,
