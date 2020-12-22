93bd3b29838b8a41c5371f58aad2eff063f34ca7
iobaidu5 <i.obaidu5@gmail.com>
2020-12-21 21:57:43 -0800

    Error handling

--- app.js
+++ app.js
@@ -38,4 +38,26 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req,res,next) => {
+  // res.status(404).json({
+  //   status: "failed",
+  //   message: "Page Not Found"
+  // });
+  const err = new Error(`Cant Find ${req.originalUrl} on this server`);
+  err.status = "Fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err,res,req,next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+  });
+});
+
 module.exports = app;
