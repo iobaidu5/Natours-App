42b60cb34d0e86ba0baaebdc7cd4d7818880440d
iobaidu5 <i.obaidu5@gmail.com>
2020-12-25 19:52:52 -0800

    Exception

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = require('./app.js');
 
 dotenv.config({ path: './config.env'});
 
+process.on('uncaughtException', err => {
+    console.log('uncaughtException! Shutting Down');
+    console.log(err.name, err.message);
+    
+});
+
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB, {
@@ -16,6 +22,14 @@ mongoose.connect(DB, {
 
 //  SERVER LISTENING
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server Listening on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+    console.log('unhandledRejection! Shutting down');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
