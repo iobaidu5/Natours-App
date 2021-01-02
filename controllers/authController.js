f203aab566ee93d2a1a20bac40ddab28e46bdcac
iobaidu5 <i.obaidu5@gmail.com>
2021-01-01 22:06:09 -0800

    User Model

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-unresolved
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.signup = catchAsync(async (req,res,next) => {
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+        });
+
+        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.EXPIRES_IN
+        })
+
+    res.status(201).json({
+        status: "success",
+        data: {
+            user: newUser
+        }
+    });
+});
