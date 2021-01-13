669916ff519a9ae299518a6e5f1a1e02b911e0e1
iobaidu5 <i.obaidu5@gmail.com>
2021-01-13 19:47:20 +0500

    Reset password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/no-unresolved
+const crypto = require('crypto');
 const { promisify }  = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
@@ -133,4 +134,33 @@ exports.forgetPassword = catchAsync(async (req,res,next) => {
     
 });
 
-exports.resetPassword = (req,res,next) => {}
+exports.resetPassword = catchAsync(async (req,res,next) => {
+
+    //1) Get user based on token
+    const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+
+    const user = await User.findOne({
+        passwordResetToken: hashedToken,
+        passwordResetExpires: { $gt: Date.now() }
+    });
+
+    // 2) If token has not expired there is user, set new password
+    if(!user) {
+        return next(new AppError('Token is invalid or has expired', 400));
+    }
+
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+
+    await user.save();
+
+    // 3) Update changedpasswordAt
+    // 4) Log the user in, Send JWT
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: "success",
+        token
+    })
+});
--- models/userModel.js
+++ models/userModel.js
@@ -56,6 +56,14 @@ userSchema.pre('save', async function(next) {
     next();
 });
 
+userSchema.pre('save', function(next) {
+    if (!this.isModified('password') || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() -1000;
+
+    next();
+});
+
 userSchema.method.correctPassword = async function(
     candidatePassword,
     userPassword
