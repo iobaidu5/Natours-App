17d072e5ced23c55b6587cdf6e253f54529eec5c
iobaidu5 <i.obaidu5@gmail.com>
2021-01-16 19:59:04 +0500

    Security

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const helmet = require('helmet');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-const app = express();
 
-app.use(express.json());
+
+const app = express();
+ 
+app.use(express.json({ limit: '10kb'}));
+
+// Data sanitization again NOSQL query Injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
+
+// Prevent Paramer pollution
+app.use(hpp({
+    whitelist: [
+      'duration',
+      "ratingsQuantity",
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+}));
+
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request, Pleasre try again in an hour"
+});
+app.use('/api', limiter);
+
+// set security HTTP headers
+app.use(helmet());
 
 app.use(express.static(`${__dirname}/public`));
 
